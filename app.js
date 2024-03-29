ce1fe145e91712c0f2481a4043edeb6d28c697c2
tjr1wns23 <tjr1wns23@naver.com>
2024-03-29 15:33:52 +0900

    to hard

---
+++ app.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Spinner from './Spinner';
+
+const App = () => {
+  return (
+    <div>
+      {/* Other components */}
+      <Spinner />
+    </div>
+  );
+};
+
+export default App;
